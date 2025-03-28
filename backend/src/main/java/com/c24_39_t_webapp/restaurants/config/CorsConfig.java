48f67cf41041a9002d5cab38baa88319734ac250
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2025-03-28 15:09:56 +0100

    Fix: Ajusta cors para permitir consultas desde el front cambiando despues solo la variable de entorno en el servidor donde este desplegada la app.

--- backend/src/main/java/com/c24_39_t_webapp/restaurants/config/CorsConfig.java
+++ backend/src/main/java/com/c24_39_t_webapp/restaurants/config/CorsConfig.java
@@ -1,24 +1,31 @@
 package com.c24_39_t_webapp.restaurants.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    //Permite ajustar la url del front añadiendo variables de entorno ALLOWED_ORIGINS
+    // a allowedOrigins en el servidor donde este desplegada la app, en este caso Railway.
+    @Value("#{'${ALLOWED_ORIGINS:*}'.split(',')}") // Si no existe la variable, usa "*"
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Cambiar ajustando al frontend
+                        .allowedOriginPatterns(allowedOrigins) // ¡Usa la variable aquí!
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
-
 }
