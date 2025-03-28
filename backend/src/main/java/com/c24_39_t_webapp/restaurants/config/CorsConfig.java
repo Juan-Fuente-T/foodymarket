61eafc4e33d566a95f601249af01093b23f1cd3a
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2025-03-28 14:29:23 +0100

    Fix: Ajusta cors para permitir consultas desde el front provisional

--- backend/src/main/java/com/c24_39_t_webapp/restaurants/config/CorsConfig.java
+++ backend/src/main/java/com/c24_39_t_webapp/restaurants/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080/**") // Cambia esto según tu frontend
+                        .allowedOrigins("*") // Cambiar ajustando al frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
